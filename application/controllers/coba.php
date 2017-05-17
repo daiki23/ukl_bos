df844e2736a33073115753e419535f800cd58a34
daiki23 <r.dikaadji@gmail>
2017-05-17 12:45:20 +0700

    commit coba

--- application/controllers/coba.php
+++ application/controllers/coba.php
@@ -1,3 +1,21 @@
+<?php
 
 
+defined('BASEPATH') OR exit('No direct script access allowed');
 
+class coba extends CI_Controller {
+
+    public function index()
+    {
+        
+        this->load->view('login_view');
+        
+        
+    }
+
+}
+
+/* End of file Controllername.php */
+
+
+?>
